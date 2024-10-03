26c632d3db5cedda012e5f8d374b55975ebc7e38
Sfdqd <skw681096@gmail.com>
2024-10-03 21:41:24 +0300

    Update afk_with_gif.py

--- afk_with_gif.py
+++ afk_with_gif.py
@@ -11,22 +11,18 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class AFKMod(loader.Module):
-    """Повідомляє інших, що ви перебуваєте в AFK і дозволяє додавати та зберігати до 5 медіа."""
+    """Повідомляє інших, що ви перебуваєте в AFK і дозволяє додавати медіа через URL."""
 
     strings = {
-        "name": "AFK with gif",
+        "name": "afk_with_gif",
         "gone": "<b>Я перейшов у режим AFK</b>",
         "back": "<b>Я більше не в режимі AFK</b>",
         "afk": "<b>Я зараз в AFK (з {} тому).</b>",
         "afk_reason": "<b>Я зараз в AFK (з {} тому).\nПричина:</b> <i>{}</i>",
         "media_installed": "<b>Медіа AFK було встановлено!</b>",
-        "media_saved": "<b>Медіа AFK збережено!</b>",
         "media_removed": "<b>Медіа AFK було видалено.</b>",
-        "media_limit_reached": "<b>Досягнуто ліміт збережених медіа (5). Видаліть одне, щоб додати нове.</b>",
-        "media_not_found": "<b>Невірний медіа URL або індекс.</b>",
-        "media_list": "<b>Збережені медіа:</b>\n\n{}",
+        "media_not_found": "<b>Невірний медіа URL для AFK.</b>",
         "afk_preview": "<b>Ось як виглядатиме ваше повідомлення AFK:</b>\n\n{}",
-        "afk_no_media": "<b>У вас немає збережених медіа.</b>",
     }
 
     async def client_ready(self, client, db):
@@ -59,69 +55,20 @@ class AFKMod(loader.Module):
         await utils.answer(message, self.strings("back", message))
 
     async def afkmediacmd(self, message):
-        """.afkmedia <media URL> - Встановити або зберегти медіа для AFK через URL"""
+        """.afkmedia <URL медіа> - Встановити або замінити медіа для AFK через URL"""
         args = utils.get_args_raw(message)
 
         if not args or not (args.startswith("http://") or args.startswith("https://")):
             await utils.answer(message, self.strings("media_not_found", message))
             return
 
-        saved_media = self._db.get(__name__, "saved_media", [])
-
-        if len(saved_media) >= 5:
-            await utils.answer(message, self.strings("media_limit_reached", message))
-            return
-
-        saved_media.append(args)
-        self._db.set(__name__, "saved_media", saved_media)
-        await utils.answer(message, self.strings("media_saved", message))
-
-    async def setafkmediacmd(self, message):
-        """.setafkmedia <index> - Встановити медіа для AFK за індексом збереженого списку"""
-        args = utils.get_args_raw(message)
-
-        if not args.isdigit():
-            await utils.answer(message, self.strings("media_not_found", message))
-            return
-
-        index = int(args) - 1
-        saved_media = self._db.get(__name__, "saved_media", [])
-
-        if index < 0 or index >= len(saved_media):
-            await utils.answer(message, self.strings("media_not_found", message))
-            return
-
-        self._db.set(__name__, "afk_media", saved_media[index])
+        # Зберігаємо вказаний медіа URL
+        self._db.set(__name__, "afk_media", args)
         await utils.answer(message, self.strings("media_installed", message))
 
-    async def listafkmediacmd(self, message):
-        """.listafkmedia - Показати список збережених медіа"""
-        saved_media = self._db.get(__name__, "saved_media", [])
-
-        if not saved_media:
-            await utils.answer(message, self.strings("afk_no_media", message))
-            return
-
-        media_list = "\n".join([f"{i+1}. {url}" for i, url in enumerate(saved_media)])
-        await utils.answer(message, self.strings("media_list", message).format(media_list))
-
     async def removeafkmediacmd(self, message):
-        """.removeafkmedia <index> - Видалити медіа з збереженого списку за індексом"""
-        args = utils.get_args_raw(message)
-
-        if not args.isdigit():
-            await utils.answer(message, self.strings("media_not_found", message))
-            return
-
-        index = int(args) - 1
-        saved_media = self._db.get(__name__, "saved_media", [])
-
-        if index < 0 or index >= len(saved_media):
-            await utils.answer(message, self.strings("media_not_found", message))
-            return
-
-        del saved_media[index]
-        self._db.set(__name__, "saved_media", saved_media)
+        """.removeafkmedia - Видалити поточний медіа URL для AFK"""
+        self._db.set(__name__, "afk_media", None)
         await utils.answer(message, self.strings("media_removed", message))
 
     async def watcher(self, message):
@@ -159,9 +106,10 @@ class AFKMod(loader.Module):
             elif afk_state is not False:
                 ret = self.strings("afk_reason", message).format(diff, afk_state)
 
+            # Відправляємо відповідь AFK разом з медіа, якщо воно є
             media = self._db.get(__name__, "afk_media")
             if media:
-                await message.reply(ret, file=media)  
+                await message.reply(ret, file=media)  # Надсилаємо медіа безпосередньо
             else:
                 await utils.answer(message, ret, reply_to=message)
 
@@ -183,7 +131,7 @@ class AFKMod(loader.Module):
 
         media = self._db.get(__name__, "afk_media")
         if media:
-            await message.reply(self.strings("afk_preview", message).format(ret), file=media)  
+            await message.reply(self.strings("afk_preview", message).format(ret), file=media)  # Надсилаємо медіа безпосередньо
         else:
             await utils.answer(message, self.strings("afk_preview", message).format(ret))
 
